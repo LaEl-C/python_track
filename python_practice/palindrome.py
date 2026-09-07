9a30ad000cbb7bf44b70ce6fa5ae1f3f5ba40451
LaEl-C <darecaroly14@gmail.com>
2026-09-07 12:57:23 +0100

    palindrome

--- python_practice/palindrome.py
+++ python_practice/palindrome.py
@@ -1,6 +1,33 @@
 def palindrome(word):
-    new_word = reversed(word)
+    word = word.lower()
+    new_word = "".join(reversed(word))
     if word == new_word:
         return True
+    return False
 
 print(palindrome("hannah"))
+print(palindrome("Hello"))
+print(palindrome("Tenet"))
+
+
+#OR
+
+def palindrome(word):
+    word = word.lower()
+    return word == word[::-1]
+
+print(palindrome("hannah"))  # True
+print(palindrome("Hello"))   # False
+print(palindrome("Tenet"))   # True
+
+
+#OR
+
+def palindrome(word):
+    word = word.lower()
+    new_word = "".join(reversed(word))
+    return word == new_word
+
+print(palindrome("hannah"))  # True
+print(palindrome("Hello"))   # False
+print(palindrome("Tenet"))   # True
